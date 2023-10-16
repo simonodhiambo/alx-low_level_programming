aeb82988c21dfcccb974fda8a02ea03373d2bacc
simonodhiambo <odhiambosimon080@gmail.com>
2023-10-16 14:11:05 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,10 @@
-#include <stdio.h>
-#include <unstd.>
+#ifndef MAIN_H
+#define MAIN_H
 
-int_putchar(char c);
+#include <stdio.h>
+#include <unistd.h>
+
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
